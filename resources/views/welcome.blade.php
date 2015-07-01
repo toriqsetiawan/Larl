4f2f116e08489b41bd416a2d7479151a695b36d9
Hossein Jabbari <mr.hhkj@gmail.com>
2015-07-02 03:52:30 +0430

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>LARL - Open Source Laravel 5 URL Shortener</title>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/foundation/stylesheets/app.css" />
+		<link rel="stylesheet" href="/foundation/stylesheets/custom.css" />
+		<script src="/foundation/bower_components/modernizr/modernizr.js"></script>
+	</head>
+	<body>
+		<div class="row">
+			<div class="small-12 column">
+				<img src="/images/logo.png">
+				<h6>Open Source Laravel 5 URL Shortener</h6>
+			</div>
+		</div>
+		<div class="row">
+			<div class="small-12 column">
+			<form>
+				<fieldset>
+					<legend class="radius">Enter the URL you want to shorten below</legend>
+						<label>
+							<input class="radius" type="url" placeholder="Example: http://www.google.com">
+						</label>
+				</fieldset>
+			</form>
+			</div>
+		</div>
+		<div class="row">
+			<div class="small-12 column">
+				<h6><a href="#"><img src="/images/github.png"> Github Project Page</a></h6>
+			</div>
+		</div>
+		<script src="/foundation/bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="/foundation/bower_components/foundation/js/foundation.min.js"></script>
+		<script src="/foundation/js/app.js"></script>
+	</body>
+</html>
